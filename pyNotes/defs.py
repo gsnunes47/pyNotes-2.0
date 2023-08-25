8b8e8a4cb8b1bfb19b864047ffb83887d3336325
Gustavo Nunes <gustavompn06@gmail.com>
2023-08-25 08:46:12 -0300

    Criação da tela de login e algumas funcionalidades
    Adição de uma função para a criação da table principal no DB e inicio da tela de login / cadastro.

--- pyNotes/defs.py
+++ pyNotes/defs.py
@@ -1,5 +1,16 @@
 import mysql.connector
 
+def dbstart():
+    con = mysql.connector.connect(host='localhost', database='users', user='root', password='')
+    cursor = con.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS LOGIN(
+	LOGIN VARCHAR(20) NOT NULL PRIMARY KEY,
+    SENHA VARCHAR(20) NOT NULL
+);""")
+
+# def signup():
+
+
 def end():
     con.close()
     cursor.close()
--- pyNotes/main.py
+++ pyNotes/main.py
@@ -1,10 +1,23 @@
 import mysql.connector
 import defs
 
+#conexão com o banco de dados
 con = mysql.connector.connect(host='localhost', database='users', user='root', password='')
 cursor = con.cursor()
 
-if con.is_connected():
-    print('working')
+defs.dbstart()
+
+print('=-'*20)
+print('Bem-vindo ao pyNotes 2.0, para iniciar faça o login ou cadastro.')
+while True:
+    print("Digite 1 para realizar o Cadastro ou 2 para fazer login.")
+    choice = str(input("Escolha: "))
+    if choice == '1':
+        login = str(input("Login [Nome]: "))
+        senha = str(input("Senha: "))
+        print('Cadastro realizado com sucesso!')
+        print('='*20)
+    if choice == '2':
+        print('login')
 
 defs.end()
