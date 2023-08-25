6cbbeb6ee2e5f5ee81ee9df5d1b920a051a51d1c
Gustavo Nunes <gustavompn06@gmail.com>
2023-08-25 10:17:34 -0300

    Adição da função de login e correção de bug na função de encerramento.
    A função verifica se os dados digitados pelo usuário existem dentro do banco de dados e caso estiver tudo certo ela encerra e o programa continua, caso contrário ela apenas volta ao início, ou se o usuário acertar o login mas errar a senha, ele tem até 3 chances para acertar a senha ou o programa encerra.
    
    Além disso descobri certos bugs na função de cadastro, fiz comentários em cima para corrigir.

--- pyNotes/defs.py
+++ pyNotes/defs.py
@@ -17,6 +17,30 @@ def signup(login, senha):
     if user is not None:
         print(f"Cadastro realizado com sucesso! Seja bem-vindo {login}.")
 
+def signin(login):
+    con = mysql.connector.connect(host='localhost', database='users', user='root', password='')
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * FROM LOGIN WHERE LOGIN = '{login}';")
+    user = cursor.fetchone()
+    if login in user:
+        strike = 0
+        while strike != 3:
+            senha = str(input("Senha: "))
+            if senha in user:
+                print(f"Login realizado com sucesso! Seja bem-vindo {login}.")
+                break
+            else:
+                strike += 1
+                if strike <= 2:
+                    print('Senha Incorreta, tente novamente.')
+                elif strike == 3:
+                    print('Você digitou a senha incorreta muitas vezes.')
+                    exit()        
+    else:
+        print('Usuário não encontrado, faça o cadastro ou tente novamente.')
+    
 def end():
+    con = mysql.connector.connect(host='localhost', database='users', user='root', password='')
+    cursor = con.cursor()
     con.close()
     cursor.close()
--- pyNotes/main.py
+++ pyNotes/main.py
@@ -8,21 +8,27 @@ cursor = con.cursor()
 #Verificação da existencia da table de usuários
 defs.dbstart()
 
-print('=-'*20)
+print('=-='*20)
 print('Bem-vindo ao pyNotes 2.0, para iniciar faça o login ou cadastro.')
 while True:
     print("Digite 1 para realizar o Cadastro ou 2 para fazer login.")
     choice = str(input("Escolha: "))
     
-    #Cadastro
+    #Cadastro /// att: Adicionar exceção pra caso o usuário tente cadastrar um login já existente 
+    # / Adicionar exceção para caso o usuário não for encontrado
     if choice == '1':
-        login = str(input("Login [Nome]: "))
+        login = str(input("Login: "))
         senha = str(input("Senha: "))
         defs.signup(login, senha)
-        print('='*20)
+        print('=-='*20)
     
     #Login
     if choice == '2':
-        print('login')
+        login = str(input("Login: "))
+        defs.signin(login)
+        print('=-='*20)
+        break
+
+print('Notas')
 
 defs.end()
