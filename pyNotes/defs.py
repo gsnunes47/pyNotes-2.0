ab4a9ed9fb2b216b9a9ca57f71ac8ef980198639
Gustavo Nunes <gustavompn06@gmail.com>
2023-08-25 12:13:56 -0300

    Correção de bugs nas funções de cadastro e login.

--- pyNotes/defs.py
+++ pyNotes/defs.py
@@ -11,34 +11,43 @@ def dbstart():
 def signup(login, senha):
     con = mysql.connector.connect(host='localhost', database='users', user='root', password='')
     cursor = con.cursor()
-    cursor.execute(f"INSERT INTO LOGIN VALUES('{login}', '{senha}');")
-    cursor.execute(f"SELECT * FROM LOGIN WHERE LOGIN = '{login}' AND SENHA = '{senha}';")
-    user = cursor.fetchone()
-    if user is not None:
-        print(f"Cadastro realizado com sucesso! Seja bem-vindo {login}.")
+    cursor.execute(f"SELECT * FROM LOGIN WHERE LOGIN = '{login}';")
+    row = cursor.fetchone()
+    if row == None:
+        cursor.execute(f"INSERT INTO LOGIN VALUES('{login}', '{senha}');")
+        cursor.execute(f"SELECT * FROM LOGIN WHERE LOGIN = '{login}' AND SENHA = '{senha}';")
+        user = cursor.fetchone()
+        if user is not None:
+            print(f"Cadastro realizado com sucesso! Seja bem-vindo {login}.")
+    else:
+        print("Login já cadastrado, tente utilizar outro nome.")
 
 def signin(login):
     con = mysql.connector.connect(host='localhost', database='users', user='root', password='')
     cursor = con.cursor()
     cursor.execute(f"SELECT * FROM LOGIN WHERE LOGIN = '{login}';")
     user = cursor.fetchone()
-    if login in user:
-        strike = 0
-        while strike != 3:
-            senha = str(input("Senha: "))
-            if senha in user:
-                print(f"Login realizado com sucesso! Seja bem-vindo {login}.")
-                break
-            else:
-                strike += 1
-                if strike <= 2:
-                    print('Senha Incorreta, tente novamente.')
-                elif strike == 3:
-                    print('Você digitou a senha incorreta muitas vezes.')
-                    exit()        
+    if user != None:
+        if login in user:
+            strike = 0
+            while strike != 3:
+                senha = str(input("Senha: "))
+                if senha in user:
+                    print(f"Login realizado com sucesso! Seja bem-vindo {login}.")
+                    break
+                else:
+                    strike += 1
+                    if strike <= 2:
+                        print('Senha Incorreta, tente novamente.')
+                    elif strike == 3:
+                        print('Você digitou a senha incorreta muitas vezes.')
+                        exit()
+                        return False        
     else:
         print('Usuário não encontrado, faça o cadastro ou tente novamente.')
-    
+        return False
+    return True
+
 def end():
     con = mysql.connector.connect(host='localhost', database='users', user='root', password='')
     cursor = con.cursor()
--- pyNotes/main.py
+++ pyNotes/main.py
@@ -14,8 +14,7 @@ while True:
     print("Digite 1 para realizar o Cadastro ou 2 para fazer login.")
     choice = str(input("Escolha: "))
     
-    #Cadastro /// att: Adicionar exceção pra caso o usuário tente cadastrar um login já existente 
-    # / Adicionar exceção para caso o usuário não for encontrado
+    #Cadastro
     if choice == '1':
         login = str(input("Login: "))
         senha = str(input("Senha: "))
@@ -25,9 +24,10 @@ while True:
     #Login
     if choice == '2':
         login = str(input("Login: "))
-        defs.signin(login)
-        print('=-='*20)
-        break
+        confirmação = defs.signin(login)
+        if confirmação == True:
+            print('=-='*20)
+            break
 
 print('Notas')
 
